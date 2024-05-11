1c3aaf211e9aafa904d2b4bd3fa87ad9b1ee6182
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-12 01:00:31 +0500

    юзкейс получения покемона

--- app/src/main/java/ru/kryu/vkpokemon/feature/pokemoninfo/domain/PokemonInfoUseCase.kt
+++ app/src/main/java/ru/kryu/vkpokemon/feature/pokemoninfo/domain/PokemonInfoUseCase.kt
@@ -1,4 +1,12 @@
 package ru.kryu.vkpokemon.feature.pokemoninfo.domain
 
-class PokemonInfoUseCase {
+import kotlinx.coroutines.flow.Flow
+import ru.kryu.vkpokemon.feature.pokemoninfo.domain.model.Pokemon
+import ru.kryu.vkpokemon.util.Resource
+import javax.inject.Inject
+
+class PokemonInfoUseCase @Inject constructor(private val pokemonInfoRepository: PokemonInfoRepository) {
+    fun getPokemonInfo(id: Int): Flow<Resource<Pokemon>> {
+        return pokemonInfoRepository.getPokemonInfo(id)
+    }
 }
