d81824bbd943b1c1e80a35e2aa0f4d266cfc814a
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-12 01:03:17 +0500

    di для информации о покемоне

--- app/src/main/java/ru/kryu/vkpokemon/di/RepositoryModule.kt
+++ app/src/main/java/ru/kryu/vkpokemon/di/RepositoryModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ru.kryu.vkpokemon.feature.pokemoninfo.data.PokemonInfoRepositoryImpl
+import ru.kryu.vkpokemon.feature.pokemoninfo.domain.PokemonInfoRepository
 import ru.kryu.vkpokemon.feature.pokemonlist.data.PokemonListRepositoryImpl
 import ru.kryu.vkpokemon.feature.pokemonlist.domain.PokemonListRepository
 import ru.kryu.vkpokemon.network.NetworkClient
@@ -17,4 +19,9 @@ class RepositoryModule {
     @Singleton
     fun providePokemonListRepository(networkClient: NetworkClient): PokemonListRepository =
         PokemonListRepositoryImpl(networkClient)
+
+    @Provides
+    @Singleton
+    fun providePokemonInfoRepository(networkClient: NetworkClient): PokemonInfoRepository =
+        PokemonInfoRepositoryImpl(networkClient)
 }
--- app/src/main/java/ru/kryu/vkpokemon/di/UseCaseModule.kt
+++ app/src/main/java/ru/kryu/vkpokemon/di/UseCaseModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import ru.kryu.vkpokemon.feature.pokemoninfo.domain.PokemonInfoRepository
 import ru.kryu.vkpokemon.feature.pokemoninfo.domain.PokemonInfoUseCase
 import ru.kryu.vkpokemon.feature.pokemonlist.domain.PokemonListRepository
 import ru.kryu.vkpokemon.feature.pokemonlist.domain.PokemonListUseCase
@@ -17,6 +18,6 @@ class UseCaseModule {
         PokemonListUseCase(pokemonListRepository)
 
     @Provides
-    fun providePokemonInfoUseCase(): PokemonInfoUseCase =
-        PokemonInfoUseCase()
+    fun providePokemonInfoUseCase(pokemonInfoRepository: PokemonInfoRepository): PokemonInfoUseCase =
+        PokemonInfoUseCase(pokemonInfoRepository)
 }
