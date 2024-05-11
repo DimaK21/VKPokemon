c029435c56a58f845c52cf491d51c84bb0285081
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-12 02:00:02 +0500

    фрагмент покемона

--- app/src/main/java/ru/kryu/vkpokemon/feature/pokemoninfo/presentation/PokemonInfoFragment.kt
+++ app/src/main/java/ru/kryu/vkpokemon/feature/pokemoninfo/presentation/PokemonInfoFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import ru.kryu.vkpokemon.R
 import ru.kryu.vkpokemon.databinding.FragmentPokemonInfoBinding
+import ru.kryu.vkpokemon.feature.pokemoninfo.domain.model.Pokemon
 
 @AndroidEntryPoint
 class PokemonInfoFragment : Fragment() {
@@ -28,6 +37,56 @@ class PokemonInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.toastLiveData.observe(viewLifecycleOwner) {
+            showToast(it)
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.screenState.collect { state ->
+                    when (state) {
+                        is PokemonInfoState.Content -> showContent(state.content)
+                        PokemonInfoState.Error -> showError()
+                        PokemonInfoState.Loading -> showLoading()
+                    }
+                }
+            }
+        }
+        binding.btnRefresh.setOnClickListener {
+            viewModel.refreshClicked()
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showContent(pokemon: Pokemon) {
+        binding.groupSuccess.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.GONE
+        binding.groupError.visibility = View.GONE
+        binding.tvName.text = pokemon.name
+        binding.tvHeight.text = getString(R.string.height, pokemon.height)
+        binding.tvWeight.text = getString(R.string.weight, pokemon.weight)
+        binding.tvForms.text = getString(R.string.forms, pokemon.forms.toString())
+        Glide.with(requireContext())
+            .load(pokemon.icon)
+            .placeholder(R.drawable.pokemon_placeholder)
+            .transform(
+                CenterCrop(),
+            )
+            .into(binding.ivIcon)
+    }
+
+    private fun showError() {
+        binding.groupSuccess.visibility = View.GONE
+        binding.progressBar.visibility = View.GONE
+        binding.groupError.visibility = View.VISIBLE
+    }
+
+    private fun showLoading() {
+        binding.groupSuccess.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+        binding.groupError.visibility = View.GONE
     }
 
     override fun onDestroy() {
